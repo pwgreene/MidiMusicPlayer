a545280e6ff556f2b285879dffb169b731c44a30
Parker Greene <pwgreene@mit.edu>
2015-11-28 17:17:10 -0500

    added a Music interface

---
+++ src/abc/player/Music.java
@@ -0,0 +1,40 @@
+package abc.player;
+
+/**
+ * A piece of music played by one or more instruments
+ *
+ */
+public interface Music {
+    
+    //Recursive Data Type Definition:
+    //Music = SingleNote(duration:double, pitch:Pitch, accidental:char) + Rest(double:duration) 
+    //        + Tuplet(notes:List<Music>) + Chord(notes:List<SingleNote>, durations:List<Integer>)
+    //        + Concat(m1:Music, m2: Music) + Layer(m1:Music, m2: Music)
+    
+    //Abstraction Function:
+    //TODO
+    //Rep Invariant:
+    //TODO
+    //Safety from Rep Exposure:
+    //TODO
+    
+    /**
+     * Transpose this music piece by a given number of semitones
+     * @param semitonesUp an integer representing the number of semitones by which to transpose
+     * this piece of music. A positive number shifts it up, a negative number shifts it down
+     * @return a new piece of music transposed by semitonesUp semitones.
+     */
+    public Music transpose(int semitonesUp);
+    
+    /**
+     * Changes all notes in the piece to the specified key
+     * @param key the key to play this piece in
+     * @return a new piece of music transposed into the specified key
+     */
+    public Music inKey(Key key);
+}
+
+enum Key {
+    C_MAJOR, G_MAJOR, D_MAJOR, A_MAJOR, E_MAJOR, B_MAJOR, Fsharp_MAJOR, Csharp_MAJOR,
+    A_MINOR, E_MINOR, B_MINOR, Fsharp_MINOR, Csharp_MINOR, Gsharp_MINOR, Dsharp_MINOR, Asharp_MINOR
+}
