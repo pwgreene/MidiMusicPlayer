836ee1a19570fb4c243b42b79ef49ddafd0aec9e
Parker Greene <pwgreene@mit.edu>
2015-12-01 14:40:57 -0500

    added more tests to Rest

--- test/abc/player/RestTest.java
+++ test/abc/player/RestTest.java
@@ -19,27 +19,46 @@ public class RestTest {
     
     @Test
     public void testRestDurationZero() {
-        Rest rest = new Rest(0);
+        Music rest = new Rest(0);
         assertEquals("", rest.toString());
         assertEquals(0, rest.getDuration());
     }
     
     @Test
     public void testRestDurationOne() {
-        Rest rest = new Rest(1);
+        Music rest = new Rest(1);
         assertEquals("-", rest.toString());
         assertEquals(1, rest.getDuration());
     }
     
     @Test
     public void testRestDurationGreaterThanOne() {
-        Rest rest = new Rest(5);
+        Music rest = new Rest(5);
         assertEquals("- - - - -", rest.toString());
         assertEquals(5, rest.getDuration());
     }
     
     //transpose():
     //transpose by semitonesUp = <0, 0, >0
+    
+    @Test
+    public void testDurationTransposeNegative() {
+        Music rest = new Rest(5);
+        Music rest2 = rest.transpose(-1);
+        assertEquals("-", rest2.toString());
+    }
+    @Test
+    public void testDurationTransposeZero() {
+        Music rest = new Rest(5);
+        Music rest2 = rest.transpose(0);
+        assertEquals("-", rest2.toString());
+    }
+    @Test
+    public void testDurationTransposePositive() {
+        Music rest = new Rest(5);
+        Music rest2 = rest.transpose(3);
+        assertEquals("-", rest2.toString());
+    }
     
     //play():
     //No way to test this?
