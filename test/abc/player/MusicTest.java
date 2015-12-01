4e6e551f60694b497bde8d83f278e01e70029494
Parker Greene <pwgreene@mit.edu>
2015-12-01 13:56:49 -0500

    added test strategy for Music and Rest. Added tests for Rest

--- test/abc/player/MusicTest.java
+++ test/abc/player/MusicTest.java
@@ -1,5 +1,17 @@
 package abc.player;
 
 public class MusicTest {
-
+    
+    /*Testing Strategy:*/
+    
+    //Music.parse() Partitions:
+    //Input file has:
+    //-no optional headers/1-4 optional headers/5 optional headers
+    //-1, >1 bars
+    //repeat/no repeat
+    //1, >1 voices
+    //Notes:
+    //-whole/half/quarter/eighth/etc.
+    //a note has/ doesn't have accidental
+    
 }
--- test/abc/player/RestTest.java
+++ test/abc/player/RestTest.java
@@ -1,5 +1,46 @@
 package abc.player;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 public class RestTest {
 
+    /*Testing Strategy*/
+    
+    //Constructor:
+    //duration = 0, 1, >1
+    
+    //toString():
+    //rest duration is 0, 1, >1
+    
+    //getDuration():
+    //rest duration is 0, 1, >1
+    
+    @Test
+    public void testRestDurationZero() {
+        Rest rest = new Rest(0);
+        assertEquals("", rest.toString());
+        assertEquals(0, rest.getDuration());
+    }
+    
+    @Test
+    public void testRestDurationOne() {
+        Rest rest = new Rest(1);
+        assertEquals("-", rest.toString());
+        assertEquals(1, rest.getDuration());
+    }
+    
+    @Test
+    public void testRestDurationGreaterThanOne() {
+        Rest rest = new Rest(5);
+        assertEquals("- - - - -", rest.toString());
+        assertEquals(5, rest.getDuration());
+    }
+    
+    //transpose():
+    //transpose by semitonesUp = <0, 0, >0
+    
+    //play():
+    //No way to test this?
 }
