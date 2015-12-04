006b1c639599e7eeb71bcb50512069cba179869e
Parker Greene <pwgreene@mit.edu>
2015-12-04 13:19:41 -0500

    got rid of voice names in Layer constructor

--- src/abc/player/Layer.java
+++ src/abc/player/Layer.java
@@ -14,20 +14,16 @@ public class Layer implements Music {
     
     private final Music m1;
     private final Music m2;
-    private final String v1Name;
-    private final String v2Name;
     
     /**
      * Create a new Layer object that joins two voice together
      * @param m1 the first voice to be played
      * @param m2 the second voice to be played
      */
-    public Layer(Music m1, Music m2, String voiceName1, String voiceName2) {
+    public Layer(Music m1, Music m2) {
         //TODO: what about the case where its m1 or m2 is also a Layer? How would you do voiceNames?
         this.m1 = m1;
         this.m2 = m2;
-        this.v1Name = voiceName1;
-        this.v2Name = voiceName2;
     }
     
     @Override
@@ -41,7 +37,7 @@ public class Layer implements Music {
  
     @Override
     public String toString() {
-        return v1Name+": "+m1.toString()+"\n"+v2Name+": "+m2.toString();
+        return m1.toString()+"\n"+m2.toString()+"\n";
     }
 
     @Override
