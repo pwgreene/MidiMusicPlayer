2a267cb1ead2f176aa9f0297d0107b53a1f36864
Brian Huang <brihuang@mit.edu>
2015-12-07 11:53:10 -0500

    added tests to MusicTest and created repeatTuple test file

--- test/abc/player/MusicTest.java
+++ test/abc/player/MusicTest.java
@@ -13,22 +13,24 @@ import org.junit.Test;
 import abc.sound.SequencePlayer;
 
 public class MusicTest {
-    
+
     /*Testing Strategy:*/
-    
+
     //Music.parse() Partitions:
     //Input file has:
-     //no optional headers/1-4 optional headers/5 optional headers
-     //1, >1 bars
-     //repeat/no repeat
-     //1, >1 voices
+    //  *no optional headers (X,T,K only)/*1-3 optional headers/*all 4 optional headers 
+    //  *1, *>1 bars
+    //  *repeat/*no repeat
+    //  *1, *>1 voices
     //Notes:
-     //-whole/half/quarter/eighth/etc.
-     //a note has/ doesn't have accidental
+    //  *whole/half/quarter/eighth/etc. (like e e/2 e/4 e/8 regardless of BPM) 
+    //  a note has/ doesn't have accidental
+    //  *middle octave, *lower octaves, *upper octaves
+    //  with/*without *chords, *tuples
     //Key:
-     //Major/Minor/sharp/flat
-    
-    @Test //no optional headers, 1 bar, no repeat, 1 voice
+    //  *Major/Minor/sharp/flat
+
+    @Test //no optional headers, 1 bar, no repeat, 1 voice, low octave, high octave, middle octave, major key, whole notes
     public void testParseSample1() throws MidiUnavailableException, InvalidMidiDataException, IOException {
         File sample1 = new File("sample_abc/sample1.abc");
         MusicAndBeat musicAndBeat = Music.parse(sample1);
@@ -44,4 +46,41 @@ public class MusicTest {
                 + "Event: NOTE_OFF Pitch: 84 Tick 6\n"
                 + "Meta event: END_OF_TRACK Tick: 6\n", player.toString());
     }
+    
+    @Test //chords
+    public void testParseSample2() throws MidiUnavailableException, InvalidMidiDataException, IOException {
+        File sample2 = new File("sample_abc/sample2.abc");
+        MusicAndBeat musicAndBeat = Music.parse(sample2);
+        SequencePlayer player = new SequencePlayer(musicAndBeat.getBeatsPerMinute(), 1); //TODO what are beatsPerMinute and ticksPerBeat?
+        musicAndBeat.getMusic().play(player, 0);
+        //TODO assertEquals(    , player.toString());
+    }
+    
+    @Test //multiple voices
+    public void testParseSample3() throws MidiUnavailableException, InvalidMidiDataException, IOException {
+        File sample3 = new File("sample_abc/sample3.abc");
+        MusicAndBeat musicAndBeat = Music.parse(sample3);
+        SequencePlayer player = new SequencePlayer(musicAndBeat.getBeatsPerMinute(), 1); //TODO what are beatsPerMinute and ticksPerBeat?
+        musicAndBeat.getMusic().play(player, 0);
+        //TODO assertEquals(    , player.toString());
+    }
+    
+    @Test //multiple bars, all 4 optional headers
+    public void testParseScale() throws MidiUnavailableException, InvalidMidiDataException, IOException {
+        File scale = new File("sample_abc/scale.abc");
+        MusicAndBeat musicAndBeat = Music.parse(scale);
+        SequencePlayer player = new SequencePlayer(musicAndBeat.getBeatsPerMinute(), 1); //TODO what are beatsPerMinute and ticksPerBeat?
+        musicAndBeat.getMusic().play(player, 0);
+        //TODO assertEquals(    , player.toString());
+    }
+    
+    @Test //tuples, repeats, 1-3 optional headers
+    public void testParserepeatTuple() throws MidiUnavailableException, InvalidMidiDataException, IOException {
+        File file = new File("sample_abc/repeatTuple.abc");
+        MusicAndBeat musicAndBeat = Music.parse(file);
+        SequencePlayer player = new SequencePlayer(musicAndBeat.getBeatsPerMinute(), 1); //TODO what are beatsPerMinute and ticksPerBeat?
+        musicAndBeat.getMusic().play(player, 0);
+        //TODO assertEquals(    , player.toString());
+    }
+    
 }
